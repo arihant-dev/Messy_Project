417c49273628478cf1c4328be53309df815dd9f2
arihant-dev <jainarj411@gmail.com>
2026-05-13 15:44:35 +0200

    feat(random-animals): add function to randomize animal data

--- src/helpers2.py
+++ src/helpers2.py
@@ -4,3 +4,8 @@ def getData():
     data = ["apple", "banana", "cherry"]
     idx = random.randint(0, len(data)-1)
     return data[idx]
+
+def getRandomizeDataAnimals():
+    data = ["lion", "tiger", "bear", "dog"]
+    idx = random.randint(0, len(data)-1)
+    return data[idx]
